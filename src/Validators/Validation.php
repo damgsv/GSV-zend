dcca21be79d50499372244e102e420ba86c8c31e
Damien <damien.galicher@galicher.fr>
2017-03-20 20:44:41 +0100

    validator

---
+++ src/Validators/Validation.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: galicher
+ * Date: 20/03/17
+ * Time: 20:38
+ */
+class Validation
+{
+
+    /**
+     * @var
+     */
+    private $errors;
+
+    /**
+     * @param $fields
+     * @return bool
+     */
+    public function isMandatory($fields)
+    {
+        $return = false;
+
+        foreach ($fields as $key => $value) {
+
+            if(null == $value) {
+
+                $this->errors[$key] = 'Le champ "' . $key . '" est obligatoire.';
+                $return = true;
+            }
+        }
+
+        return $return;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+}
